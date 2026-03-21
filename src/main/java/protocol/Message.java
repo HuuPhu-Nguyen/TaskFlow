da729dde9de94495d84dc9c3ce71e253782cc9ca
Phu Nguyen <nguyehuu@dickinson.edu>
2026-03-21 01:47:38 -0400

    added setter for Message pojo

--- src/main/java/protocol/Message.java
+++ src/main/java/protocol/Message.java
@@ -4,4 +4,16 @@ public class Message {
     String type;
     String nodeId;
     String time;
+
+    public String getNodeId() {
+        return nodeId;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getTime() {
+        return time;
+    }
 }
